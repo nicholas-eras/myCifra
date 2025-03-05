83994f82906cb11fc4a90e6913332bef90ac7782
Nicholasmais <nicoeras@hotmail.com>
2025-03-05 19:08:28 -0300

    vercel 7 added /dist

---
+++ back/dist/music/dto/music.dto.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreateMusicDto = exports.UpdateMusicDto = void 0;
+;
+class UpdateMusicDto {
+}
+exports.UpdateMusicDto = UpdateMusicDto;
+class CreateMusicDto {
+}
+exports.CreateMusicDto = CreateMusicDto;
+//# sourceMappingURL=music.dto.js.map
