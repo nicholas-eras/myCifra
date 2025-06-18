3eab433b8229dd99d7d63d6755525829aa40a5ce
Nicholas Eras <nicoeras@hotmail.com>
2025-06-18 14:58:37 -0300

    swap chords finering

--- front/components/ChordDiagram.tsx
+++ front/components/ChordDiagram.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { findGuitarChord } from "chord-fingering";
 
 
 const ChordDiagram = ({ chordName } : {chordName : string}) => {
+  const result = findGuitarChord(chordName);
+  const [chordIndex, setChordIndex] = useState<number>(0);
+
+  const swapChord = (increment: number) => {
+    const total = result.fingerings.length;
+    const newIndex = (chordIndex + increment + total) % total;
+    setChordIndex(newIndex);
+  };
+
+  const positions = result.fingerings[chordIndex].positions;
+  const maxFret = Math.max(...positions.map((fing) => fing.fret));
+  let minFret = 0;
+  for (let i = 0; i <= 5; i++) {
+    const stringFingering = positions.find(p => p.stringIndex === i);
+    if (stringFingering && stringFingering.fret > 0) {
+      minFret = stringFingering.fret;
+      break;
+    }
+  }
+
+  const diagramFingering = positions.map((fing) => {
+    let fret = fing.fret;
+    if (maxFret > 6){
+      if (fret > 6){
+        fret -= 6;
+      }
+    }
+    return {
+      xIndex: fing.stringIndex,
+      yIndex: fret,
+      fretNotation: maxFret
+    }
+  });
+  console.log(positions);
+  console.log(diagramFingering);
   const svgSize = 250;
   const maxWidthRel = 100;
   const maxHeightRel = 100;
@@ -28,23 +63,6 @@ const ChordDiagram = ({ chordName } : {chordName : string}) => {
     x2: 100
   }));
   
-  const result = findGuitarChord(chordName);
-  const positions = result.fingerings[2].positions;
-  const maxFret = Math.max(...positions.map((fing) => fing.fret));
-  let minFret = Math.min(...positions.map((fing) => fing.fret));
-  
-  const diagramFingering = positions.map((fing) => {
-    let fret = fing.fret;
-    if (maxFret > 6){
-      fret -= 6;
-    }
-    return {
-      xIndex: fing.stringIndex,
-      yIndex: fret,
-      fretNotation: maxFret
-    }
-  });
-  console.log(positions);
   return (
     <div>
       <svg 
@@ -79,7 +97,7 @@ const ChordDiagram = ({ chordName } : {chordName : string}) => {
 
           {horizontalLines.map((square, i) => {
             if (i === 0 ){
-              return maxFret > 6 && <line x1={square.x1} y1={square.y1} x2={square.x2} y2={square.y2} stroke='black' strokeWidth={lineWidth} key={`horizonal-${i}`}/>
+              return <line x1={square.x1} y1={square.y1} x2={square.x2} y2={square.y2} stroke='black' strokeWidth={maxFret > 6 ? lineWidth : lineWidth / 2} key={`horizonal-${i}`}/>
             }
             else{
               return <line x1={square.x1} y1={square.y1} x2={square.x2} y2={square.y2} stroke='black' strokeWidth={i === 0 ? 1 : lineWidth} key={`horizonal-${i}`}/>
@@ -88,6 +106,9 @@ const ChordDiagram = ({ chordName } : {chordName : string}) => {
           )}
 
           {diagramFingering.map((circle, i) => {
+            if (circle.yIndex < 0){
+              circle.yIndex = 0;
+            }
             const cx = circle.xIndex*squareWidth + circle.xIndex * lineWidth + squareWidth / 2 - lineWidth / 2;
             const cy = circle.yIndex*squareHeight + circle.yIndex * lineWidth + squareHeight / 2 - lineWidth / 2;
             return (              
@@ -103,6 +124,28 @@ const ChordDiagram = ({ chordName } : {chordName : string}) => {
             );
           })}
       </svg>
+
+      <div className="button-row" style={{display: "flex", flexDirection: "row"}}>
+        <div
+          className="first-svg-space"
+          style={{
+            width: `${squareWidth / 100 * svgSize}px`,
+            height: `${squareHeight}px`
+          }}>
+        </div>
+        <div
+          className="swap-fingering"
+          style={{
+            width: `${svgSize}px`,
+            display: "flex",
+            flexDirection:"row",
+            alignItems: "center",
+            justifyContent: "center"
+          }}>
+          <button onClick={()=>swapChord(-1)}>{"<"}</button>
+          <button onClick={()=>swapChord(1)}>{">"}</button>
+        </div>
+      </div>
     </div>
   )
 }
