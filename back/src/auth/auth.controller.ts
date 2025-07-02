5724fa9269c9653664f3dcfbbd570bf55e1090c4
Nicholas Eras <nicoeras@hotmail.com>
2025-07-01 23:28:31 -0300

    fix lax cookie settings

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -30,7 +30,7 @@ constructor(
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production', 
-            sameSite: 'lax',
+            sameSite: 'none',
             maxAge: 1000 * 60 * 60 * 24, // 1 dia
         });
  
