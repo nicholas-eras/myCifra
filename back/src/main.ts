9d1235d42546bff69aa801b7599a810912f2fbc4
Nicholas Eras <nicoeras@hotmail.com>
2025-07-01 23:23:50 -0300

    require cookie parser

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,17 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser';
+const cookieParser = require('cookie-parser'); 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
 
   app.enableCors({
-    origin: process.env.ENVIRONMENT_FRONTEND ?? "http://localhost:3001",
+    origin: process.env.ENVIRONMENT_FRONTEND ?? 'http://localhost:3001',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: ['Origin', 'Content-Type', 'Accept'],
     credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
